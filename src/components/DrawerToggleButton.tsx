7d936df4f93ee4015072ed251c9c88a5f34b88e6
metastable-void <git@yuka.h09.eu>
2026-03-05 18:22:50 +0900

    hamburger menu

--- src/components/DrawerToggleButton.tsx
+++ src/components/DrawerToggleButton.tsx
@@ -1,15 +1,11 @@
 
 import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { drawerSlice } from '../ui/drawer';
-import { RootState } from '../app/store';
 
 export const DrawerToggleButton = () => {
   const dispatch = useDispatch();
-  const drawerIsOpen = useSelector((state: RootState) => state.drawer.open);
-  const direction = useSelector((state: RootState) => state.direction.direction);
-  const sidePanel = direction == 'ltr' ? 'left_panel' : 'right_panel';
 
   return (
     <button
@@ -17,6 +13,6 @@ export const DrawerToggleButton = () => {
       onClick={() => {
         dispatch(drawerSlice.actions.toggleDrawer());
       }}
-    >{sidePanel + (drawerIsOpen ? '_close' : '_open')}</button>
+    >{'menu'}</button>
   );
 };
